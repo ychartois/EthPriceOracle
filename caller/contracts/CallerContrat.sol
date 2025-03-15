fbd68363deb56140c569b6dde9df98fee3f556bf
ychartois <ychartois@gmail.com>
2025-03-15 12:07:31 +0000

    add modifier to protect call of the callback

--- caller/contracts/CallerContrat.sol
+++ caller/contracts/CallerContrat.sol
@@ -15,6 +15,11 @@ contract CallerContract is Ownable{
     event ReceivedNewRequestIdEvent(uint256 id);
     event PriceUpdatedEvent(uint256 ethPrice, uint256 id);
 
+    modifier onlyOracle() {
+      require(msg.sender == oracleAddress, "You are not authorized to call this function.");
+      _;
+    }
+
     function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
@@ -27,7 +32,7 @@ contract CallerContract is Ownable{
         emit ReceivedNewRequestIdEvent(id);
     }
 
-    function callback(uint256 _ethPrice, uint256 _id) public {
+    function callback(uint256 _ethPrice, uint256 _id) public onlyOracle {
         require(myRequests[_id], "This request is not in my pending list.");
         ethPrice = _ethPrice;
         delete myRequests[_id];
