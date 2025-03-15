9fe56300ff2051700aba08770eed5859abc4b07f
ychartois <ychartois@gmail.com>
2025-03-15 13:33:16 +0000

    processing the event

--- EthPriceOracle.js
+++ EthPriceOracle.js
@@ -9,3 +9,32 @@ const CHUNK_SIZE = process.env.CHUNK_SIZE || 3
 const MAX_RETRIES = process.env.MAX_RETRIES || 5
 
 var pendingRequests = []
+
+async function getOracleContract (web3js) {
+    const networkId = await web3js.eth.net.getId()
+    return new web3js.eth.Contract(OracleJSON.abi, OracleJSON.networks[networkId].address)
+}
+
+async function filterEvents (oracleContract, web3js) {
+    oracleContract.events.GetLatestEthPriceEvent(async (err, event) => {
+      if (err) {
+        console.error('Error on event', err)
+        return
+      }
+      await addRequestToQueue(event)
+    })
+  
+    oracleContract.events.SetLatestEthPriceEvent(async (err, event) => {
+      if (err) {
+        console.error('Error on event', err)
+        return
+      }
+     
+    })
+}
+
+async function addRequestToQueue (event) {
+    const callerAddress = event.returnValues.callerAddress
+    const id = event.returnValues.id
+    pendingRequests.push({ callerAddress, id })
+}
