75a813c38c037a2c43094c542a1d4498144b9d44
ychartois <ychartois@gmail.com>
2025-03-24 16:05:47 +0000

    add functions to retrieve and set the latest ETH price in EthPriceOracle

--- EthPriceOracle.js
+++ EthPriceOracle.js
@@ -15,6 +15,17 @@ async function getOracleContract (web3js) {
     return new web3js.eth.Contract(OracleJSON.abi, OracleJSON.networks[networkId].address)
 }
 
+async function retrieveLatestEthPrice () {
+	const resp = await axios({
+	  url: 'https://api.binance.com/api/v3/ticker/price',
+	  params: {
+		symbol: 'ETHUSDT'
+	  },
+	  method: 'get'
+	})
+	return resp.data.price
+}
+
 async function filterEvents (oracleContract, web3js) {
     oracleContract.events.GetLatestEthPriceEvent(async (err, event) => {
       if (err) {
@@ -64,3 +75,35 @@ async function processRequest(oracleContract, ownerAddress, id, callerAddress) {
 		}
 	}
 }
+
+async function setLatestEthPrice (oracleContract, callerAddress, ownerAddress, ethPrice, id) {
+	ethPrice = ethPrice.replace('.','');
+	const multiplier = new BN(10**10, 10);
+	const ethPriceInt = (new BN(parseInt(ethPrice), 10)).mul(multiplier)
+	const idInt = new BN(parseInt(id))
+	try {
+	  	await oracleContract.methods.setLatestEthPrice(ethPriceInt.toString(), callerAddress, idInt.toString()).send({ from: ownerAddress })
+	} catch (error) {
+	  	console.log('Error encountered while calling setLatestEthPrice.')
+		console.error('Error details:', error);
+	}
+}
+
+async function init() {
+    const { ownerAddress, web3js, client } = common.loadAccount(PRIVATE_KEY_FILE_NAME);
+    const oracleContract = await getOracleContract(web3js);
+    await filterEvents(oracleContract, web3js);
+    return { oracleContract, ownerAddress, client };
+}
+
+(async () => {
+	const { oracleContract, ownerAddress, client } = await init()
+	process.on( 'SIGINT', () => {
+	  	console.log('Calling client.disconnect()');
+		client.disconnect();
+	  	process.exit();
+	})
+	setInterval(async () => {
+		await processQueue(oracleContract, ownerAddress);
+	}, SLEEP_INTERVAL)
+})()
